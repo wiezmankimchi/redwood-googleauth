8091222a85ff9e0801830dad5444c5daebdfae86
Bennett Rogers <bennettrogers@gmail.com>
2020-05-12 19:50:50 -0400

    wrap app in auth provider

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom';
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web';
 import FatalErrorPage from 'src/pages/FatalErrorPage';
+import {
+  GoogleAuthProvider,
+  useGoogleAuth,
+} from 'src/contexts/GoogleAuthContext';
 
 import Routes from 'src/Routes';
 
@@ -8,9 +12,11 @@ import 'src/styles/index.scss';
 
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider>
-      <Routes />
-    </RedwoodProvider>
+    <GoogleAuthProvider>
+      <RedwoodProvider useAuth={useGoogleAuth}>
+        <Routes />
+      </RedwoodProvider>
+    </GoogleAuthProvider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
 );
